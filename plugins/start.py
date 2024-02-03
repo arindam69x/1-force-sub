6f3462316934e95efa6c7b0dfa5909333b1f6c51
arindam69x <147473276+arindam69x@users.noreply.github.com>
2024-02-03 12:32:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -55,7 +55,7 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("Please wait Vro...")
         try:
             messages = await get_messages(client, ids)
         except:
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("❤️⚡ About Me", callback_data = "about"),
+                    InlineKeyboardButton("☠️ Close", callback_data = "close")
                 ]
             ]
         )
@@ -123,7 +123,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
+                "⚡Join Channel⚡",
                 url = client.invitelink)
         ]
     ]
@@ -131,7 +131,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = '😴Try Again😴',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
